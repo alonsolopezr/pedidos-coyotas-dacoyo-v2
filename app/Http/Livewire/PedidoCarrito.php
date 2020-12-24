527837f8250dc2140cdf160c3f0947c2df0c6647
JOSE ALONSO LOPEZ ROMO <alonso.lopez.r@gmail.com>
2020-12-24 00:10:17 -0700

    pedido carrito livewire, falta save
    Signed-off-by: JOSE ALONSO LOPEZ ROMO <alonso.lopez.r@gmail.com>

---
+++ app/Http/Livewire/PedidoCarrito.php
@@ -0,0 +1,121 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+use App\Models\Producto;
+
+class PedidoCarrito extends Component
+{
+    //vars
+    public $productos;
+    public $pedido;
+    public $cuantosArticulos;
+    public $montoTotal;
+
+    public function render()
+    {
+        //cargar los productos
+        $this->productos = Producto::all();
+        //actualizar carrito
+        $this->contarArticulos();
+        $this->calculaMontoTotal();
+        return view('livewire.pedido-carrito');
+    }
+
+    public function contarArticulos()
+    {
+        $this->cuantosArticulos = isset($this->pedido) ? count($this->pedido) : 0;
+    }
+
+
+    public function mount()
+    {
+        //despues render????
+        //actualizar carrito
+        $this->contarArticulos();
+    }
+
+    public function agregarACarrito($prodId, $cantidad)
+    {
+        //parsear prodId
+        $prodId = str_replace("coyo", "", $prodId);
+        $prod = $this->obtenerProductoDeArray($prodId);
+        //dd($this->pedido, $prodId, $cantidad, $prod->precio);
+        //si ya está en el carrito, actualiza cantidad
+        if ($this->siExisteActualiza($prodId, $cantidad) == null)
+        {
+            //agregar a carrito por cantidades
+            // $this->pedido[$this->cuantosArticulos] = [
+            $this->pedido[$prodId] = [
+                "id" => $prodId,
+                "nombre" => $prod->nombre,
+                "descripcion" => $prod['descripcion'],
+                "precio" => $prod['precio'],
+                "imagen_1" => $prod['imagen_1'],
+                "cantidad" => $cantidad,
+                //TODO: necesitamos mas??
+            ];
+        }
+        $this->calculaMontoTotal();
+        //dd($this->pedido, $prodId, $cantidad, $prod);
+    }
+
+    public function obtenerProductoDeArray($prodId)
+    {
+        foreach ($this->productos as $key => $prod)
+        {
+
+            # code...
+            // dd($prod->id, $prodId);
+            if ($prod->id == $prodId)
+            {
+                return $prod;
+            }
+        }
+        return null;
+    }
+    public function siExisteActualiza($prodId, $cantidad)
+    {
+        if (isset($this->pedido))
+            foreach ($this->pedido as $key => $prod)
+            {
+                // si se encuentra el producto en el carrito
+                if ($prod['id'] == $prodId)
+                {
+                    //si cantidad es 0, quitar del arreglo
+                    if ($cantidad == 0)
+                        $this->quitarDelCarrito($key);
+                    else
+                    {
+                        $this->pedido[$key]['cantidad'] = $cantidad;
+                    }
+
+                    return true;
+                    // dd($this->pedido, $prod['id'], $prodId, $this->pedido[$key]['cantidad'], $cantidad);
+                }
+            }
+        return null;
+    }
+    public function quitarDelCarrito($idx)
+    {
+        //quitar elemento del carrito por idx
+        unset($this->pedido[$idx]);
+    }
+
+
+    public function calculaMontoTotal()
+    {
+        $this->montoTotal = 0;
+        if ($this->pedido != null)
+            foreach ($this->pedido as $key => $articulo)
+            {
+                $this->montoTotal += $articulo['cantidad'] * $articulo['precio'];
+            }
+    }
+
+    public function registrarPedido()
+    {
+        //hacer el store
+    }
+}
--- resources/views/pedidos/create.blade.php
+++ resources/views/pedidos/create.blade.php
@@ -1,59 +1,12 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            PEDIDOS
+            TU PEDIDO
             {{-- {{ __('Dashboard') }} --}}
         </h2>
     </x-slot>
-    <div class=" flex flex-wrap mx-2 justify-center">
-        <div class="py-12">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                    <form action="{{route('pedidos.store')}}" method="POST">
-                        @csrf
-                    <!-- This is an example component -->
-                        <div id="menu" class="container mx-auto px-4 lg:pt-24 lg:pb-64">
-                            <div class="flex flex-wrap text-center justify-center">
-                                <div class="w-full lg:w-6/12 px-4">
-                                    <h2 class="text-4xl font-semibold text-black">Seleccione sus productos</h2>
-                                    <p class="text-lg leading-relaxed mt-4 mb-4 text-gray-500">
-                                        Seleccione los paquetes de Coyotas, de nuestras delicias rellenas de piloncillo, jamoncillo y jamoncillo con nuez.
-                                    </p>
-                                </div>
-                            </div>
 
-                            <hr>
-                            <div class="flex flex-wrap mt-12 justify-center">
-                            @foreach ($productos as $producto)
-                                <div class="grid grid-cols-1 sm:grid-cols-6 md:grid-cols-6 lg:grid-cols-6 xl:grid-cols-6 gap-4">
-                                    <div class="col-span-2 sm:col-span-1 xl:col-span-1">
-                                        <img
-                                            alt="{{$producto->nombre}}"
-                                            src="{{asset('storage/'.$producto->imagen_1)}}"
-                                            class="h-24 w-24 rounded  mx-auto" />
-                                    </div>
-                                    <div class="col-span-2 sm:col-span-4 xl:col-span-4">
-                                        <h3 class="font-semibold text-black">{{$producto->nombre}}</h3>
-                                        <p>
-                                        {{$producto->descripcion}}
-                                        </p>
-                                    </div>
-                                    <div class="col-span-2 sm:col-span-1 xl:col-span-1 italic ">
-                                        <div class="flex"> ${{$producto->precio}} <input class="px-6 bg-gray-300 rounded mx-4"  type="number" value="1" name="coyo{{$producto->id}}" id="coyo{{$producto->id}}" size="2" min="0" max="18"></div>
-                                    </div>
-                                </div>
-                            @endforeach
-                            </div>
-                            <hr>
-                            <button class="bg-red-600 p-3 m-2 rounded text-bold" type="submit">Ordenar</button>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-        @livewire('carrito')
+        @livewire('pedido-carrito')
         {{-- , ['user' => $user], key($user->id)) --}}
-    </div>
-
 </x-app-layout>
 
