1e04168faec22cf0bc692494916cfa0c7e906ec6
JOSE ALONSO LOPEZ ROMO <alonso.lopez.r@gmail.com>
2020-12-30 23:37:52 -0700

    index pedidos mejor labels status, falta email...falta leer y detalle de qr
    Signed-off-by: JOSE ALONSO LOPEZ ROMO <alonso.lopez.r@gmail.com>

--- resources/views/pedidos/index.blade.php
+++ resources/views/pedidos/index.blade.php
@@ -14,7 +14,7 @@
                             <td class="p-3 font-bold uppercase bg-coyos-lightblue text-gray-600 border-r-4 border-coyos-darkblue text-center lg:table-cell">Folio</td>
                             <td class="p-3 font-bold uppercase bg-coyos-lightblue text-gray-600 border-r-4 border-coyos-darkblue text-center lg:table-cell">Registrado el</td>
                             <td class="p-3 font-bold uppercase bg-coyos-lightblue text-gray-600 border-r-4 border-coyos-darkblue text-center lg:table-cell">Entrega para</td>
-                            <td class="p-3 font-bold uppercase bg-coyos-lightblue text-gray-600 border-r-4 border-coyos-darkblue text-center lg:table-cell">Productos</td>
+                            <td class="p-3 font-bold uppercase bg-coyos-lightblue text-gray-600 border-r-4 border-coyos-darkblue text-center lg:table-cell">¿Qué ordenaste?</td>
                             <td class="p-3 font-bold uppercase bg-coyos-lightblue text-gray-600 border-r-4 border-coyos-darkblue text-center lg:table-cell">Sucursal</td>
                             <td class="p-3 font-bold uppercase bg-coyos-lightblue text-gray-600 border-r-4 border-coyos-darkblue text-center lg:table-cell">Status</td>
                             <td class="p-3 font-bold uppercase bg-coyos-lightblue text-gray-600 border-r-4 border-coyos-darkblue text-center lg:table-cell">Cancelar</td>
@@ -29,19 +29,46 @@
                                 <td class="px-2 text-center border-solid border-r-4 border-coyos-darkblue w-max-content ">{{$pedido->fecha.'@'.$pedido->hora}}</td>
                                 <td class="px-2 text-center border-solid border-r-4 border-coyos-darkblue">{{$pedido->paquetes_de_coyotas}}</td>
                                 <td class="px-2 text-center border-solid border-r-4 border-coyos-darkblue">{{$pedido->sucursal}}</td>
-                                <td class="px-2 text-center border-solid border-r-4 border-coyos-darkblue">{{$pedido->status}}</td>
-                                <td class="px-2 text-center border-solid border-r-4 border-coyos-darkblue">
-                                    <form action="">
-                                        <button class="text-center align-middle hover:bg-coyos-lightblue mx-2 hover:text-coyos-darkpink bg-coyos-darkpink text-coyos-lightblue rounded text-xl px-2 py-2 text-bold">¿Cancelar?</button>
-                                    </form>
-                                </td>
-                                <td class="flex flex-row border-solid border-r-4 border-coyos-darkpink">
-                                    <form action="" class="flex flex-row max-h-full">
-                                        <textarea name="msg" id="msg" cols="30" rows="5">
-                                            Escribir comentario a enviar a cliente
-                                        </textarea>
-                                        <button class="min-h-full text-center hover:bg-coyos-darkblue -pt-10  hover:text-coyos-lightyellow bg-coyos-lightyellow text-coyos-darkblue rounded text-xl  text-bold">Enviar</button>
-                                    </form></td>
+                                @php
+                                    $clases="";
+                                    if ($pedido->status=='REGISTRADO')
+                                    {
+                                        $clases="px-2 text-center bg-orange-400 text-coyos-darkblue rounded text-xl  text-bold ";
+                                    }else if($pedido->status=='CONFIRMADO')
+                                    $clases="px-2 text-center bg-blue-400 text-coyos-darkblue rounded text-xl  text-bold  ";
+                                    else if($pedido->status=='EN_PROCESO')
+                                    $clases="px-2 text-center bg-yellow-600 text-coyos-darkblue rounded text-xl  text-bold  ";
+                                    else if($pedido->status=='PREPARADO')
+                                    $clases="px-2 text-center bg-teal-600 text-coyos-lightyellow rounded text-xl  text-bold  ";
+                                    else if($pedido->status=='EMPAQUETADO')
+                                    $clases="px-2 text-center bg-coyos-midbrown text-coyos-darkblue rounded text-xl  text-bold  ";
+                                    else if($pedido->status=='EMPAQUETADO')
+                                    $clases="px-2 text-center bg-coyos-midbrown text-coyos-darkblue rounded text-xl  text-bold  ";
+                                    else if($pedido->status=='ESPERANDO_CLIENTE')
+                                    $clases="px-2 text-center bg-coyos-lightyellow text-coyos-darkblue rounded text-xl  text-bold  ";
+                                    else if($pedido->status=='ENTREGADO')
+                                    $clases="px-2 text-center bg-green-500 text-coyos-darkblue rounded text-xl  text-bold  ";
+                                @endphp
+                                <td class="px-2 border-solid border-r-4 border-coyos-darkblue" ><p class="{{$clases}}">{{$pedido->status}}</p></td>
+
+                                @if ($pedido->status=='ENTREGADO')
+                                    <td colspan="2" class="px-2 py-4 text-center border-solid border-r-4  border-coyos-darkpink">
+                                        <p class="text-center align-middle text-xl-center mx-2 text-green-700  text-2xl px-2 py-2 text-bold ">Pedido entregado y disfrutado.</p>
+                                    </td>
+                                @else
+                                    <td class="px-2 text-center border-solid border-r-4 border-coyos-darkblue">
+                                        <form action="">
+                                            <button class="text-center align-middle shadow-outline-indigo  hover:bg-coyos-lightblue mx-2 hover:text-coyos-darkpink bg-coyos-darkpink text-coyos-lightblue rounded text-xl px-2 py-2 text-bold">¿Cancelar?</button>
+                                        </form>
+                                    </td>
+                                    <td class="flex flex-row border-solid border-r-4 border-coyos-darkpink">
+                                        <form action="" class="flex flex-row max-h-full">
+                                            <textarea class="bg-cool-gray-300 rounded " name="msg" id="msg" cols="30" rows="5" placeholder="¿quieres comentar algo sobre tu pedido?">
+
+                                            </textarea>
+                                            <button class="min-h-full text-center hover:bg-coyos-darkblue -pt-10  hover:text-coyos-lightyellow bg-coyos-lightyellow text-coyos-darkblue rounded text-xl  text-bold">Enviar</button>
+                                        </form></td>
+                                @endif
                             </tr>
                         @endforeach
                     </tbody>
